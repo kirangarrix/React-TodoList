501b565f8c448ecf3dcba32c00f1ff71c8776bee
kiran garrix <kirangarrix2019@gmail.com>
2021-07-27 12:53:39 +0530

    Todo using react

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+        <p>Todolist app 2021 &copy;</p>
+            
+        </div>
+    )
+}
+
+export default Footer
